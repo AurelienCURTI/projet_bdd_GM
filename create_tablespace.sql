0af49d0aaa8150371ca47995fa4d18bb427058d9
AurelienCURTI <curti.aurelien@free.fr>
2017-01-28 19:28:51 +0100

    Augmentation de la taille du tablespace tbs_user

--- create_tablespace.sql
+++ create_tablespace.sql
@@ -10,8 +10,8 @@ DATAFILE '/home/aurelien/Documents/M1IFI/tbs_oradata/projectbdd/tbs_indexes.dbf'
 EXTENT MANAGEMENT LOCAL AUTOALLOCATE;
 
 CREATE TABLESPACE tbs_users
-DATAFILE '/home/aurelien/Documents/M1IFI/tbs_oradata/projectbdd/tbs_user.dbf' SIZE 5M
-EXTENT MANAGEMENT LOCAL UNIFORM SIZE 100k;
+DATAFILE '/home/aurelien/Documents/M1IFI/tbs_oradata/projectbdd/tbs_user.dbf' SIZE 300M
+EXTENT MANAGEMENT LOCAL UNIFORM SIZE 1M;
 
 CREATE TEMPORARY TABLESPACE tbs_temp
 TEMPFILE '/home/aurelien/Documents/M1IFI/tbs_oradata/projectbdd/tbs_temp.dbf' SIZE 5M
